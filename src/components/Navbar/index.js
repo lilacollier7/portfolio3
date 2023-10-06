7859d9e43bb418dfb3f42447201675bc131b9768
vintha92 <141485349+lilacollier7@users.noreply.github.com>
2023-10-06 00:55:23 -0700

    hector

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -26,8 +26,8 @@ const Navbar = () => {
           <NavLink href="#about">About</NavLink>
           <NavLink href='#skills'>Skills</NavLink>
           <NavLink href='#experience'>Experience</NavLink>
-          <NavLink href='#projects'>Projects</NavLink>
-          <NavLink href='#education'>Education</NavLink>
+          <NavLink href='#projects'>Videos</NavLink>
+          <NavLink href='#education'></NavLink>
         </NavItems>
         <ButtonContainer>
           <GitHubButton href={Bio.github} target="_blank">Youtube Profile</GitHubButton>
@@ -43,13 +43,13 @@ const Navbar = () => {
             }}>Skills</MobileLink>
             <MobileLink href='#experience' onClick={() => {
               setIsOpen(!isOpen)
-            }}>Experience</MobileLink>
+            }}> Experience</MobileLink>
             <MobileLink href='#projects' onClick={() => {
               setIsOpen(!isOpen)
-            }}>Projects</MobileLink>
+            }}>Videos</MobileLink>
             <MobileLink href='#education' onClick={() => {
               setIsOpen(!isOpen)
-            }}>Education</MobileLink>
+            }}></MobileLink>
             <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Youtube Profile</GitHubButton>
           </MobileMenu>
         }
